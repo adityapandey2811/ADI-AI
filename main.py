e31d1408ebd7f87ea6097f8aa6d9811f8510fac4
Aditya Pandey <79079234+adityapandey2811@users.noreply.github.com>
2021-06-10 19:57:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,35 +8,35 @@ def speechToText():
         try:
             text = v1.recognize_google(v1.listen(source)).lower()
         except sr.UnknownValueError:
-            print('Error: Unclear Pronounciation\nTry Again in a bit')
+            print('Error: Unclear Pronounciation\nTry Again in a bit\n')
             exit()
         except sr.RequestError:
-            print('Error: Network Issue\nTry Again in a bit')
+            print('Error: Network Issue\nTry Again in a bit\n')
             exit()
     return text
 
 
-print('Say something to search!\nLike --> "Search Dinosaurs"')
+print('Say something to search!\nLike --> "Search Dinosaurs"\n')
 text = speechToText()
 print(text)
 index = text.find("search ")
 if index == -1:
-    print('Wanna search: YES or NO')
+    print('Wanna search: YES or NO\n')
     text = speechToText()
     print(text)
     if text.find("no") >= 0:
         exit()
     elif text.find("yes") >= 0:
-        print('Try Searching!!!')
+        print('Try Searching!!!\n')
         text = speechToText()
         print(text)
     else:
-        print('Man u can\'t even speak correct\n\t  GO FUCK OFF!!!')
+        print('Man u can\'t even speak correct\n\t  GO FUCK OFF!!!\n')
         exit()
 
 index = text.find("search ")
 if index == -1:
-    print('Man u wasting my time\nTry again later')
+    print('Man u wasting my time\nTry again later\n')
     exit()
 
 text = text[index + 7:]
